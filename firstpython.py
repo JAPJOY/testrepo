30965d13db591827abbcb01f305c559ee494b13f
Jalil <87331420+JAPJOY@users.noreply.github.com>
2021-09-01 11:42:05 +0200

    Update firstpython.py
    chnage the line-3 to print("New Python File + new change")

--- firstpython.py
+++ firstpython.py
@@ -1,5 +1,5 @@
 # This is a test python file for creating a file in my new test repository.
 
-print("New Python File")
+print("New Python File + new change")
 
 print("Second Changes have been done!")
